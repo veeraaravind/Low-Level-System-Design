3771b35168b5a3603e3d22b3fa7fc0197a7406ae
Veera Aravind <36047225+veeraaravind@users.noreply.github.com>
2024-04-03 01:06:59 +0530

    Create Payroll-System-Liskov-Substitution.php

---
+++ Payroll-System-Liskov-Substitution.php
@@ -0,0 +1,37 @@
+interface Payroll {
+    public function taxCalculation(float $baseSalary): float;
+    public function getSalary(): float;
+    public function getBaseSalary(): float;
+}
+
+class SouthRegion implements Payroll {
+    protected int $totalHours;
+    protected float $perHourCost;
+    protected int $taxPercentage = 30;
+
+    public function __construct(int $totalHours, float $perHourCost) {
+        $this->totalHours = $totalHours;
+        $this->perHourCost = $perHourCost;
+    }
+
+    public function taxCalculation(float $baseSalary): float {
+        return ($baseSalary / 100) * $this->taxPercentage;
+    }
+
+    public function getBaseSalary(): float {
+        return $this->totalHours * $this->perHourCost;
+    }
+
+    public function getSalary(): float {
+        $baseSalary = $this->getBaseSalary();
+        return $baseSalary - $this->taxCalculation($baseSalary);
+    }
+}
+
+class FullTimeSouthRegionEmployee extends SouthRegion {
+    public function __construct(float $perHourCost) {
+        parent::__construct(240, $perHourCost);
+    }
+}
+
+echo (new FullTimeSouthRegionEmployee(1500))->getSalary();
